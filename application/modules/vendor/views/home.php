7a3e92d62b76212930820622e3cf3b4f966f7c8c
zhenguanyimin <736166185@qq.com>
2024-03-24 09:50:31 +0800

    去掉highchart模块

--- application/modules/vendor/views/home.php
+++ application/modules/vendor/views/home.php
@@ -131,55 +131,5 @@
                 </a>
             </div>
         </div>        
-    </div>       
-    <script src="<?= base_url('assets/highcharts/highcharts.js') ?>"></script>
-    <div id="container-by-month" style="min-width: 310px; height: 400px; margin: 0 auto;"></div>
-    <script>
-        /*
-         * Chart for orders by mount/year 
-         */
-        $(function () {
-            Highcharts.chart('container-by-month', {
-                title: {
-                    text: 'Monthly Orders',
-                    x: -20
-                },
-                subtitle: {
-                    text: 'Source: Orders table',
-                    x: -20
-                },
-                xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                },
-                yAxis: {
-                    title: {
-                        text: 'Orders'
-                    },
-                    plotLines: [{
-                            value: 0,
-                            width: 1,
-                            color: '#808080'
-                        }]
-                },
-                tooltip: {
-                    valueSuffix: ' Orders'
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle',
-                    borderWidth: 0
-                },
-                series: [
-<?php foreach ($ordersByMonth['years'] as $year) { ?>
-                        {
-                            name: '<?= $year ?>',
-                            data: [<?= implode(',', $ordersByMonth['orders'][$year]) ?>]
-                        },
-<?php } ?>
-                ]
-            });
-        });
-    </script>
+    </div>
 </div>
